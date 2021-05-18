e8514c473044358dbc7e94838698e6d98796cb8a
Chhour Thaisan <sanezzy17@gmail.com>
2021-05-18 14:39:14 +0700

    heroku private-tutoring-frontend

--- server.js
+++ server.js
@@ -1,33 +1,13 @@
-const express = require("express");
+const express = require('express')
 const serveStatic = require('serve-static')
 const path = require('path')
-const mongoose = require('mongoose')
 
-const dotenv = require('dotenv');
+const app = express()
 
-dotenv.config();
+app.use('/', serveStatic(path.join(__dirname, '/dist'))
+)
 
-const { Client } = require('mongoose');
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
-client.connect();
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err;
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row));
-  }
-  client.end();
-});
+const port = process.env.PORT || 8080
+app.listen(port)
 
-const app = express();
-app.use('/', serveStatic(path.join(__dirname, '/dist')))
-
-// set port, listen for requests
-const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
-});
+console.log('Listening on port: ' + port)
